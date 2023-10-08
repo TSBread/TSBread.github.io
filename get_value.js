a7e39d7d8a4763db6b3834f20ce9e4524786aba7
TSBread <2638318806@qq.com>
2023-10-08 22:56:52 +0800

    重置TSBread.github.io

---
+++ get_value.js
@@ -0,0 +1,32 @@
+let url = window.location.href;
+let element_start = url.indexOf("?");
+let element = url.slice(element_start + 1);
+let value_start = element.indexOf("=");
+let value = element.slice(value_start + 1);
+let decode = atob(value);
+let storage = JSON.parse(decode);
+console.log("", storage);
+let file_name = "value";
+let info = decode;
+let content = btoa(info);
+let token = "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";
+let myHeaders = { Authorization: "token " + token };
+let myData = {
+  message: "Player <" + storage["name"] + "> Update",
+  content: content,
+};
+fetch("https://api.github.com/repos/TSBread/CGAC/contents/" + file_name, {
+  method: "PUT",
+  headers: myHeaders,
+  mode: "cors",
+  body: JSON.stringify(myData),
+}).then((response) => {
+  if (response.ok) {
+    javascript: window.opener = null;
+    window.open("", "_self");
+    window.open("about:blank", "_self").close();
+  } else {
+    alert("当前上传拥挤，请再次尝试\n\n点击确定刷新当前页面");
+    window.location.reload();
+  }
+});
--- test/test2.js
+++
@@ -1,4 +0,0 @@
-let add1 = parseInt(prompt("加数1"));
-let add2 = parseInt(prompt("加数2"));
-let result = add1 + add2;
-alert('结果为'+result);
